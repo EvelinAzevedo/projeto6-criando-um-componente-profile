58ac1d79e7f57c7f6c4b4825b4b118b4b867e243
Evelin <evelinmariabarroso@gmail.com>
2025-04-30 14:54:06 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Pessoa from "./components/Pessoa"
+
+export default function App(){
+  return(
+    <div>
+     <img src="https://avatars.githubusercontent.com/u/178331348?s=96&v=4"/>
+     <h1 classname="nome">Aline</h1>
+        <p classname="idade">17 anos</p>
+        <p classname="profissao">granjeira</p>
+       <hr/>
+       <img src="https://avatars.githubusercontent.com/u/178834670?s=96&v=4"/>
+       <h2 classname="nome">Thais</h2>
+    </div>
+  )
+}
---
+++ src/components/Pessoa/index.jsx
@@ -0,0 +1,9 @@
+export default function Pessoa({nome,idade,profissao}) {
+return(
+    <div>
+       <h1>{nome}</h1>
+       <p>{idade}</p>
+       <p>{profissao}</p>
+    </div>
+)
+}
